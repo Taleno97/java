4e5c82af955a16cdca5f3fe5ba7d5e47d245e495
Taleno97 <69543154+Taleno97@users.noreply.github.com>
2020-09-18 20:39:49 +0200

    First functional version of the Calculator

--- src/IngoCalculator.java
+++ src/IngoCalculator.java
@@ -2,29 +2,50 @@ import java.util.Scanner;
 
 public class IngoCalculator{
 
+        static int Zahl1,Zahl2,rechnung,ergebnis,nocheinmal;
+        static Scanner input = new Scanner(System.in);
+
+
     public static void main (String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Bitte bestimmen Sie Ihre Rechenart:");
+        System.out.println("WAS GEHTN INGO");
+        do{
+            rechner();
+        }while(nocheinmal==1);
+        System.out.println("Nerd");
+    }
+
+    public static int rechenOperation(int a,int b){
+        switch(rechnung){
+            case 1:
+                return a + b;
+            case 2:
+                return a * b;
+            case 3:
+                return a - b;
+            case 4:
+                return a / b;
+            default:
+                System.out.println("Error!");
+                break;
+        }
+        return 0;
+    }
+
+    public static int rechner(){
+        System.out.println("Bitte bestimm die Rechenart:");
         System.out.println("1 zum addieren");
         System.out.println("2 zum multiplizieren");
         System.out.println("3 zum subtrahieren");
         System.out.println("4 zum dividieren");
-        int rechnung = Integer.parseInt(input.nextLine());
-        bestimmeRechenOperation();
-    }
-
-    public static int bestimmeRechenOperation(){
-
-        switch(rechnung){
-            case 1:
-                int ergebnis = Zahl1 + Zahl2;
-                return ergebnis;
-            case 2:
-
-            case 3:
-
-            case 4:
-        }
+        rechnung = Integer.parseInt(input.nextLine());
+        System.out.println("Danke bitte gib nun die erste Zahl ein!");
+        Zahl1 = Integer.parseInt(input.nextLine());
+        System.out.println("Danke bitte gib nun die zweite Zahl ein!");
+        Zahl2 = Integer.parseInt(input.nextLine());
+        ergebnis = rechenOperation(Zahl1,Zahl2);
+        System.out.println("Dein Ergebnis ist:" + ergebnis + " Möchtest du noch eine andere Zahl berechnen?");
+        System.out.println("1: ja  2: nein");
+        nocheinmal = Integer.parseInt(input.nextLine());
+        return nocheinmal;
     }
 }
-
