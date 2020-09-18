1fbe07f4cee10f4662f0cf4a358b5c8d9d4e7633
Lennart Talaga <talaga@mail.exwe.de>
2020-09-18 14:04:51 +0200

    Rechenweg Anfang

--- src/IngoCalculator.java
+++ src/IngoCalculator.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
 
 public class IngoCalculator{
+
     public static void main (String[] args){
-        System.out.println("HelloWorld");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bitte bestimmen Sie Ihre Rechenart:");
+        System.out.println("1 zum addieren");
+        System.out.println("2 zum multiplizieren");
+        System.out.println("3 zum subtrahieren");
+        System.out.println("4 zum dividieren");
+        int rechnung = Integer.parseInt(input.nextLine());
+        bestimmeRechenOperation();
+    }
+
+    public static int bestimmeRechenOperation(){
+
+        switch(rechnung){
+            case 1:
+                int ergebnis = Zahl1 + Zahl2;
+                return ergebnis;
+            case 2:
+
+            case 3:
+
+            case 4:
+        }
     }
 }
 
